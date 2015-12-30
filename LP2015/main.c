7d66e09171c1d9231de9873620189412821778c3
vsctiago <vsctiago@yahoo.com>
2015-12-30 11:51:20 +0000

    Revert "asd"
    This reverts commit b8cea616af990b96a4338f3917180ecad3092f1d.

--- LP2015/main.c
+++ LP2015/main.c
@@ -18,7 +18,7 @@
  * 
  */
 int main(int argc, char** argv) {
-    asdasd
+
     printf("e que mano? ");
     printf("e que mano? ");printf("e que mano? ");printf("e que mano? ");printf("e que mano? ");printf("e que mano? ");printf("e que mano? ");printf("e que mano? ");
     
