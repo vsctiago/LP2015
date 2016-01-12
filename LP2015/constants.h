994521e1f1ddf60c7b9eac5c8cc4dbb6e59a6b4f
alenet1992 <alexandre.tiago@hotmail.com>
2016-01-12 22:17:18 +0000

    #inicio

---
+++ LP2015/constants.h
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   constants.h
+ * Author: Alexandre
+ *
+ * Created on 12 de Janeiro de 2016, 21:52
+ */
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+#define TAM_STRING_NOME 50
+#define TAM_MIN_ID 11
+#define TAM_MAX_ID 11
+#define TAM_MIN_IDADE 18
+#define TAM_MAX_IDADE 90
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CONSTANTS_H */
+
